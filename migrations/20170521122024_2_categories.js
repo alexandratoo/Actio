3354e2076f06f289f14e5aa10be02937d26257eb
rhysgoehring <rhysgoehring@gmail.com>
2017-05-21 12:38:40 -0600

    migrations complete

---
+++ migrations/20170521122024_2_categories.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('categories' , (table) => {
+    table.increments();
+    table.string('title', 255).notNullable();
+  });
+};
+exports.down = function(knex) {
+  return knex.schema.dropTable('categories')
+};
