78c1076a56b5f7147842debaf6b4b12fbb72c571
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-22 12:59:46 -0600

    routes events

---
+++ server/routes/api.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const auth = require('./auth');
+const users = require('./users');
+const users = require('./events');
+const express = require('express'); /* eslint-disable new-cap */
+const router = express.Router();    /* eslint-enable new-cap */
+
+router.use('/auth', auth);
+router.use('/users', users);
+router.use('/events', events);
+
+
+module.exports = router;
