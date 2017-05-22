9340fd632d15f2739cbf0baa998719801de9739e
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-22 13:51:28 -0600

    ahhh routes

--- routes/events.js
+++ routes/events.js
@@ -6,42 +6,53 @@ const router = express.Router();            /* eslint-enable new-cap */
 /* CREATE */
 router.post('/', (req, res, next) => {
   const newEvent = req.body;
-  db.createEvent(newEvent)
+  return knex('2_events')
+  .returning('*')
+  .insert(newEvent)
   .then(() => res.sendStatus(200))
-  .catch((err) => next(err));
+  .catch((err)=> next(err));
 });
 
 /* READ */
 router.get(/:id, (req, res, next) => {
   const id = req.params.id;
-  db.readEvent(id)
+  return knex('2_events')
+  .select('*')
+  .where('id', id)
   .first()
-  .then((posture) => res.json(posture))
-  .catch((err) => next(err));
+  .then((event)=> res.json(event))
+  .catch((err)=> next(err));
 });
 
 /* UPDATE */
 router.put('/:id', (req, res, next) => {
   const id = req.params.id;
   const changes = req.body;
+return knex('2_events')
+.returning('*')
+.where('id', id)
+.update(changes)
+.then(()=> res.sendStatus(200))
+.catch((err)=> next (err));
 
-  db.updateEvent(id, changes)
-  .then(() => res.sendStatus(200))
-  .catch((err) => next(err));
 });
 
 /* DELETE */
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-  db.deleteEvent(id)
-  .then(() => res.sendStatus(200))
-  .catch((err) => next(err));
+return knex('2_events')
+.where('id', id).del()
+.then(()=> res.sendStatus(200))
+.catch((err)=> next(err))
 });
 
 /* LIST */
-router.get(`/`, (req, res, next) => {
-  db.listEvent()
-  .then((postures) => res.json(postures))
+router.get('/', (req, res, next) => {
+
+  return knex('2_events')
+  .returning('*')
+  .orderBy('created_at', 'asc')
+  .then((events) => res.json(events))
   .catch((err) => next(err));
 });
 
