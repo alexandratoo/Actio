192195493d87b980df099de207199a27a554712e
JohnG585 <jgallagher585@gmail.com>
2017-05-22 13:13:15 -0600

    user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const knex = require('../knex.js');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  return knex('1_users')
+    .select('*')
+    .then((users) => res.json(users))
+      .catch((err) => next(err));
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  return knex('1_users')
+    .select('*')
+    .where('id', id)
+    .first()
+    .then((user) => res.json(user))
+      .catch((err) => next(err));
+});
+
+router.post('/', (req, res, next) => {
+  const newUser = req.body;
+  return knex('1_users')
+    .returning('*')
+    .insert(newUsers);
+    .then(() => res.sendStatus(200))
+    .catch((err) => next(err));
+});
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const editUser = req.body;
+  return knex('1_users')
+    .returning('*')
+    .where('id', id).update(changes)
+    .then(() => res.sendStatus(200))
+    .catch((err) => next(err));
+});
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;
+  return knex('1_users')
+  .where('id', id).del()
+  .then(() => res.sendStatus(200))
+  .catch((err) => next(err));
 });
 
 module.exports = router;
