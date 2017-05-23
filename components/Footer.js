d1e314a82aea19f5431899c87de858916f5736c8
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-23 15:40:11 -0600

    changed footer to black

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
 
+var style = {
+    backgroundColor: "black",
+    borderTop: "1px solid #E7E7E7",
+    textAlign: "center",
+    padding: "20px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "60px",
+    width: "100%",
+};
+
+var phantom = {
+  display: 'block',
+  padding: '20px',
+  height: '60px',
+  width: '100%',
+}
 export default class Footer extends Component {
   render() {
     return (
-      <footer className = "footer">
-        <div className = "container">
-          <span className = "text-muted">Copyright &copy; ACTIO</span>
-        </div>
-      </footer>
+      <div>
+                <div style={phantom} />
+                <div style={style}>
+                    {this.props.children}
+                </div>
+            </div>
     );
   }
 }
