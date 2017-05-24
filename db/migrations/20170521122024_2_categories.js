ce3f485e93afb2a22e53ae7450dcfa182c3a83b7
JohnG585 <jgallagher585@gmail.com>
2017-05-24 09:20:12 -0600

    db change and icons

--- db/migrations/20170521122024_2_categories.js
+++ db/migrations/20170521122024_2_categories.js
@@ -4,6 +4,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('categories' , (table) => {
     table.increments();
     table.string('title', 255).notNullable();
+    table.string('icon', 255).notNullable();
   });
 };
 exports.down = function(knex) {
