05c951338c79f83a3a08fc1c0626a7885473f091
rhysgoehring <rhysgoehring@gmail.com>
2017-05-24 13:35:13 -0600

    typo

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@
 
 const index = require('./index');
 const users = require('./users');
-const categories = require('./catagories');
+const categories = require('./categories');
 const express = require('express'); /* eslint-disable new-cap */
 const router = express.Router();    /* eslint-enable new-cap */
 router.use('/', index);
