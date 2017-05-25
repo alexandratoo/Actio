ee6fe4a0a731a3a059961fe01229fe90af8d695d
spkelly <spkelly18@gmail.com>
2017-05-24 19:15:08 -0600

    added signup route to api file

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const index = require('./index');
 const users = require('./users');
 const categories = require('./categories');
 const messages = require('./messages');
+const signup = require('./signup.js')
 const events = require('./events')
 const express = require('express');
 const router = express.Router();    /* eslint-enable new-cap */
@@ -13,6 +14,7 @@ router.use('/users', users);
 router.use('/categories', categories);
 router.use('/messages', messages);
 router.use('/events', events);
+router.use('/signup', signup)
 
 
 module.exports = router;
