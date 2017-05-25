136cf7d44867ee7f25b8816ec746dd8d8d9297e1
spkelly <spkelly18@gmail.com>
2017-05-25 12:39:44 -0600

    facebook oAuth now creates new user and redirects to profile page

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,8 @@ const messages = require('./messages');
 const events = require('./events')
 const express = require('express');
 const router = express.Router();    /* eslint-enable new-cap */
-const signin = require('./signin')
+const signin = require('./signin');
+const fbAuth = require('./fbAuth');
 
 
 router.use('/', index);
@@ -16,6 +17,7 @@ router.use('/categories', categories);
 router.use('/messages', messages);
 router.use('/events', events);
 router.use('/signin', signin);
+router.use('/fbauth', fbAuth);
 
 
 module.exports = router;
