14dc482784fbd20cdcc317a3d2340289ace24029
sammzamm <sammzamm01@gmail.com>
2017-05-25 14:25:18 -0600

    main.js debugged

--- components/Main.js
+++ components/Main.js
@@ -6,15 +6,11 @@ import FacebookLogin from './FacebookLogin'
 export default class Main extends Component {
   render() {
   return (
-    <div class="card-block loginContent container col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
+    <div className="card-block loginContent container col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
       <h2>Welcome to ACTIO</h2>
       <FacebookLogin  fb={FB}/>
-      <div>
       <SignIn />
-      <div>
-
     </div>
-    </div>
-  );
+  )
  }
 }
