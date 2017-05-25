e8e48cc26990de4bd3fc912d931ebb8b7dd4d10b
JohnG585 <jgallagher585@gmail.com>
2017-05-25 13:07:58 -0600

    fixed single pages. working on CSS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   <BrowserRouter>
     <div className="main">
       <Route exact path="/" component={Main}></Route>
-      <Route path="/events" component={Events}></Route>
+      <Route exact path="/events" component={Events}></Route>
       <Route exact path="/events/:id" component={EventView}></Route>
       <Route exact path="/users/:id" component={Profile}></Route>
       <Route exact path="/signup" component={Signup}></Route>
