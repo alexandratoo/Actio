598cab0aecc28a8be8d453c018d8e43b9e230586
JohnG585 <jgallagher585@gmail.com>
2017-05-25 16:07:35 -0600

    CSS for single page

--- components/Main.js
+++ components/Main.js
@@ -9,11 +9,9 @@ export default class Main extends Component {
     <div class="card-block loginContent container col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
       <h2>Welcome to ACTIO</h2>
       <FacebookLogin  fb={FB}/>
-      <div>
-      <SignIn />
-      <div>
 
-    </div>
+      <SignIn />
+
     </div>
   );
  }
