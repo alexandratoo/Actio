22166c81e64a88235fea1f52978b25283a228a35
spkelly <spkelly18@gmail.com>
2017-05-24 19:37:05 -0600

    fixing api router issue

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@ const index = require('./index');
 const users = require('./users');
 const categories = require('./categories');
 const messages = require('./messages');
-const signup = require('./signup.js')
 const events = require('./events')
 const express = require('express');
 const router = express.Router();    /* eslint-enable new-cap */
@@ -14,7 +13,6 @@ router.use('/users', users);
 router.use('/categories', categories);
 router.use('/messages', messages);
 router.use('/events', events);
-router.use('/signup', signup)
 
 
 module.exports = router;
