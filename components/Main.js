3d499dec83ccb9bd0a0dc0fe2cd295043667f542
Alexandra Tooley <sandratooley@rocketmail.com>
2017-05-25 13:51:16 -0600

    main css

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-
-import Footer from './Footer'
+import SignIn from './signin'
 import { Link } from 'react-router-dom'
 import FacebookLogin from './FacebookLogin'
 
@@ -10,7 +9,11 @@ export default class Main extends Component {
     <div class="card-block loginContent container col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
       <h2>Welcome to ACTIO</h2>
       <FacebookLogin  fb={FB}/>
-      <Footer/>
+      <div>
+      <SignIn />
+      <div>
+
+    </div>
     </div>
   );
  }
