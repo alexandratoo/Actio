d0564d7a7dbe6d72240696bc4a83cffc464e6686
spkelly <spkelly18@gmail.com>
2017-05-26 00:53:16 -0600

    navbar now links to profile page

--- components/Nav.js
+++ components/Nav.js
@@ -3,6 +3,24 @@ import { Link } from 'react-router-dom';
 
 
 export default class Nav extends Component {
+  constructor(props){
+    super(props)
+    this.state = {userId:''}
+    this.parseCookie = this.parseCookie.bind(this);
+  }
+  parseCookie(cookie){
+    let cookieArray = cookie.split('; ')
+    let cookieObject = {}
+    cookieArray.forEach((element)=>{
+      let current = element.split('=');
+      cookieObject[current[0]] = current[1];
+    })
+    this.setState({userId:cookieObject.userId});
+  }
+  componentDidMount(){
+    this.parseCookie(document.cookie);
+  }
+
   render() {
     return (
       <nav className="navbar navbar-default navbar-static-top">
@@ -13,7 +31,7 @@ export default class Nav extends Component {
 
             <ul className="nav navbar-nav">
               <li><Link to="/">Home</Link></li>
-              <li><Link to="/users/1">My Profile</Link></li>
+              <li><Link to={`/users/${this.state.userId}`}>My Profile</Link></li>
               <li><Link to="/events">Find Events</Link></li>
             </ul>
           </div>
